5ad5730587f5550b4a890bac73b3551c019638ff
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-06-06 22:59:50 +0530

    initialize app with className added

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import {  Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <SafeAreaView className ='flex-1 justify-center items-center'>
+      <Text className = 'text-5xl font-bold'>
+        Hello World
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
